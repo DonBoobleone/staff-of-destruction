b07af281bb20400702113b91af3d57547a4061fe
Don Boobleone <donboobleone@gmail.com>
2026-01-13 18:08:09 +0100

    pentagram and balance

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -6,11 +6,11 @@ data:extend({
         energy_required = 30,
         ingredients = {
             { type = "item", name = "low-density-structure", amount = 20 },
-            { type = "item", name = "processing-unit", amount = 100 },
-            { type = "item", name = "battery",         amount = 200 }
+            { type = "item", name = "processing-unit",       amount = 100 },
+            { type = "item", name = "battery",               amount = 200 }
         },
         results = {
-            {type = "item", name = "staff-of-destruction", amount = 1}
+            { type = "item", name = "staff-of-destruction", amount = 1 }
         }
     },
     {
@@ -19,13 +19,13 @@ data:extend({
         enabled = false,
         energy_required = 10,
         ingredients = {
-            { type = "item", name = "explosives",     amount = 200 },
-            { type = "item", name = "uranium-235",    amount = 20 },
-            { type = "item", name = "uranium-238",    amount = 100 },
-            { type = "item", name = "energy-shield-equipment",    amount = 1 }
+            { type = "item", name = "explosives",              amount = 200 },
+            { type = "item", name = "uranium-235",             amount = 2 },
+            { type = "item", name = "uranium-238",             amount = 20 },
+            { type = "item", name = "energy-shield-equipment", amount = 1 }
         },
         results = {
-            {type = "item", name = "explosion-core", amount = 1}
+            { type = "item", name = "explosion-core", amount = 1 }
         }
     }
 })
